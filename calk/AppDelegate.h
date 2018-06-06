927344f51d6d0550b770df673a9e09e89b7699e4
Maksim <maksim@imac-imac.local>
2018-06-06 12:44:53 +0300

    Initial Commit

---
+++ calk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calk
+//
+//  Created by Maksim  on 06.06.2018.
+//  Copyright © 2018 Maksim . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
